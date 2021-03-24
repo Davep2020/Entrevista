0bb0e511edc8f89953003922d126478f106965c5
Davep2020 <119190206@castrocarazo.ac.cr>
2021-03-24 08:53:47 -0600

    Test

---
+++ Prueba/Controllers/PersonasController.cs
@@ -0,0 +1,205 @@
+﻿using Prueba.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Prueba.Controllers
+{
+    public class PersonasController : Controller
+    {
+        EntrevistaEntities modeloBD = new EntrevistaEntities();
+        #region Json
+        public ActionResult RetornaProvincias()
+        {
+            List<sp_RetornaProvincias_Result> provincias = this.modeloBD.sp_RetornaProvincias(null).ToList();
+            return Json(provincias);
+        }
+        public ActionResult RetornaCantones(int id_Provincia)
+        {
+            List<sp_RetornaCantones_Result> cantones = this.modeloBD.sp_RetornaCantones(null, id_Provincia).ToList();
+            return Json(cantones);
+        }
+
+        public ActionResult RetornaDistritos(int id_Canton)
+        {
+            List<sp_RetornaDistritos_Result> distritos = this.modeloBD.sp_RetornaDistritos(null, id_Canton).ToList();
+            return Json(distritos);
+        }
+        #endregion
+        #region Listar
+        public ActionResult PersonaLista()
+        {
+            List<sp_RetornaPersona_Result> modeloVista =
+                new List<sp_RetornaPersona_Result>();
+
+            modeloVista = modeloBD.sp_RetornaPersona().ToList();
+            return View(modeloVista);
+        }
+        #endregion
+
+        #region Eliminar
+        public ActionResult PersonaEliminar(int id_Persona)
+        {
+            sp_RetornaPersonaID_Result modelovista = new sp_RetornaPersonaID_Result();
+            modelovista = this.modeloBD.sp_RetornaPersonaID(id_Persona).FirstOrDefault();
+            ProvinciasViewbag();
+            CantonViewbag();
+            DistritoViewbag();
+            return View(modelovista);
+        }
+
+        [HttpPost]
+        public ActionResult PersonaEliminar(sp_RetornaPersonaID_Result modelovista)
+        {
+            int cantRegistrosAfectados = 0;
+            string resultado = "";
+            try
+            {
+                cantRegistrosAfectados = this.modeloBD.sp_EliminaPersona(
+                    modelovista.Id_Persona);
+            }
+            catch (Exception error)
+            {
+                resultado = "Ocurrio un error: " + error.Message;
+            }
+            finally
+            {
+                if (cantRegistrosAfectados > 0)
+                {
+                    resultado += "Registro Eliminado";
+                }
+                else
+                {
+                    resultado += "No se pudo Eliminado";
+                }
+
+            }
+            Response.Write("<script language=javascript>alert('" + resultado + "')</script>");
+            ProvinciasViewbag();
+            CantonViewbag();
+            DistritoViewbag();
+            return View(modelovista);
+
+
+        }
+        #endregion
+
+        #region Registrar
+        public ActionResult PersonaNueva()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult PersonaNueva(sp_RetornaPersonas_Result modeloVista)
+        {
+            int cantRegistroAfectados = 0;
+            string resultado = "";
+            try
+            {
+                cantRegistroAfectados = this.modeloBD.sp_RegistraPersona(
+                                        modeloVista.Nombre,
+                                        modeloVista.Correo,
+                                        modeloVista.id_Provincia,
+                                        modeloVista.id_Canton,
+                                        modeloVista.id_Distrito);
+            }
+            catch (Exception error)
+            {
+                resultado = "Ocurrio un error: " + error.Message;
+            }
+            finally
+            {
+                if (cantRegistroAfectados > 0)
+                {
+                    resultado += "Registro Insertado";
+                }
+                else
+                {
+                    resultado += "No se pudo insertar";
+                }
+            }
+
+            Response.Write("<script language=javascript>alert('" + resultado + "');</script>");
+            return View();
+        }
+        #endregion
+
+        #region Modificar
+        /// <summary>
+        /// Función Para modificar Datos 
+        /// </summary>
+        /// <param name="Id_Cliente_P"></param>
+        /// <returns></returns>
+        public ActionResult PersonaModifica(int id_Persona)
+        {
+            sp_RetornaPersonaID_Result modelovista = new sp_RetornaPersonaID_Result();
+            modelovista = this.modeloBD.sp_RetornaPersonaID(id_Persona).FirstOrDefault();
+            
+            ProvinciasViewbag();
+            CantonViewbag();
+            DistritoViewbag();
+            return View(modelovista);
+
+        }
+
+        [HttpPost]
+        public ActionResult PersonaModifica(sp_RetornaPersonaID_Result modelovista)
+        {
+            int cantRegistrosAfectados = 0;
+            string resultado = "";
+            try
+            {
+                cantRegistrosAfectados = this.modeloBD.sp_ModificaPersona(
+                    modelovista.Id_Persona,
+                    modelovista.Nombre,
+                    modelovista.Correo,
+                    modelovista.Id_Provincia,
+                    modelovista.Id_Canton,
+                    modelovista.Id_Distrito
+                    );
+            }
+            catch (Exception error)
+            {
+                resultado = "Ocurrio un error: " + error.Message;
+            }
+            finally
+            {
+                if (cantRegistrosAfectados > 0)
+                {
+                    resultado += "Registro modificado";
+                }
+                else
+                {
+                    resultado += "No se pudo modificar";
+                }
+
+            }
+            Response.Write("<script language=javascript>alert('" + resultado + "')</script>");
+            ProvinciasViewbag();
+            CantonViewbag();
+            DistritoViewbag();
+            return View(modelovista);
+
+
+        }
+
+        #endregion
+
+
+        #region Metodos
+        void ProvinciasViewbag()
+        {
+            this.ViewBag.ListaProvincia = this.modeloBD.sp_RetornaProvincias("");
+        }
+        void CantonViewbag()
+        {
+            this.ViewBag.ListaCanton = this.modeloBD.sp_RetornaCantones("", null);
+        }
+        void DistritoViewbag()
+        {
+            this.ViewBag.ListaDistrito = this.modeloBD.sp_RetornaDistritos("", null);
+        }
+        #endregion
+    }
+}
---
+++ Prueba/Scripts/Json/Geografia.js
@@ -0,0 +1,128 @@
+﻿$(function () {
+
+    estableceEventosChange();
+
+    cargaDropdownListProvincias();
+});
+
+
+function estableceEventosChange() {
+    $("#provincia").change(function () {
+        var provincia = $("#provincia").val();
+        cargaDropdownListCantones(provincia);
+    });
+    $("#canton").change(function () {
+        var canton = $("#canton").val();
+        cargarDropdownListDistritos(canton);
+    });
+}
+
+
+
+function cargaDropdownListProvincias() {
+
+    var url = '/Personas/RetornaProvincias';
+
+    var parametros = {
+    };
+
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        type: 'post',
+        contentType: 'application/json',
+        data: JSON.stringify(parametros),
+        success: function (data, textStatus, jQxhr) {
+            procesarResultadoProvincias(data);
+        },
+        error: function (jQxhr, textStatus, errorThrown) {
+            alert(errorThrown);
+        },
+    });
+}
+
+
+function procesarResultadoProvincias(data) {
+    var ddlProvincia = $("#provincia");
+    ddlProvincia.empty();
+    var nuevaOpcion = "<option value=''>Seleccione una provincia</option>";
+    ddlProvincia.append(nuevaOpcion);
+
+    $(data).each(function () {
+        var provinciaActual = this;
+        nuevaOpcion = "<option value='" + provinciaActual.id_Provincia + "'>" + provinciaActual.nombre + "</option>";
+        ddlProvincia.append(nuevaOpcion);
+    });
+}
+
+
+function cargaDropdownListCantones(pIdProvincia) {
+
+
+    var url = '/Personas/RetornaCantones';
+
+    var parametros = {
+        id_Provincia: pIdProvincia
+    };
+  
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        type: 'post',
+        contentType: 'application/json',
+        data: JSON.stringify(parametros),
+        success: function (data, textStatus, jQxhr) {
+            procesarResultadoCantones(data);
+        },
+        error: function (jQxhr, textStatus, errorThrown) {
+            alert(errorThrown);
+        },
+    });
+}
+
+
+function procesarResultadoCantones(data) {
+    var ddlCantones = $("#canton");
+    ddlCantones.empty();
+    nuevaOpcion = "<option value=''>Seleccione un Cantón</option>";
+    ddlCantones.append(nuevaOpcion);
+    $(data).each(function () {
+        var cantonActual = this;
+        nuevaOpcion = "<option value='" + cantonActual.id_Canton + "'>" + cantonActual.nombre + "</option>";
+        ddlCantones.append(nuevaOpcion);
+    });
+}
+
+function cargarDropdownListDistritos(pId_Canton) {
+
+    var url = '/Personas/RetornaDistritos';
+
+    var parametros = {
+        id_Canton: pId_Canton
+    };
+
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        type: 'post',
+        contentType: 'application/json',
+        data: JSON.stringify(parametros),
+        success: function (data, textStatus, jQxhr) {
+            procesarResultadoDistritos(data);
+        },
+        error: function (jQxhr, textStatus, errorThrown) {
+            alert(errorThrown);
+        },
+    });
+}
+function procesarResultadoDistritos(data) {
+    var ddlDistritos = $("#distrito");
+    ddlDistritos.empty();
+    nuevaOpcion = "<option value=''>Seleccione un Distrito</option>";
+    ddlDistritos.append(nuevaOpcion);
+    $(data).each(function () {
+        var DistritoActual = this;
+        nuevaOpcion = "<option value='" + DistritoActual.id_Distrito + "'>" + DistritoActual.nombre + "</option>";
+        ddlDistritos.append(nuevaOpcion);
+    });
+}
